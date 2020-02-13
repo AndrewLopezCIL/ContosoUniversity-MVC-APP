6a6ba0bd7ff410e1104b01c17e5d3442a05817ae
lopez82 <lopez82@JB202-L10-W-D>
2020-02-13 13:37:06 -0500

    First commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace ContosoUniversity.Controllers
             return View();
         }
 
+        public ActionResult Display()
+        {
+            return View(db.Students.ToList());
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
